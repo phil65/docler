6f1039fcfbcd7dae86e113b7e683a4ebafd03d17
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2025-03-24 12:48:35 +0100

    chore: add  vector db configs to state

--- src/docler/streamlit_app/state.py
+++ src/docler/streamlit_app/state.py
@@ -2,9 +2,21 @@
 
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
+
+from pydantic import Field
 from streambricks import State
 
 from docler.models import ChunkedDocument, Document  # noqa: TC001
+
+
+if TYPE_CHECKING:
+    from docler.configs.vector_db_configs import (
+        ChromaConfig,
+        OpenAIVectorConfig,
+        PineconeConfig,
+        QdrantConfig,
+    )
 
 
 class SessionState(State):
@@ -16,6 +28,9 @@ class SessionState(State):
     vector_provider: str | None = None
     uploaded_chunks: int | None = None
     chunks: list[str] | None = None
+    vector_configs: dict[
+        str, ChromaConfig | OpenAIVectorConfig | PineconeConfig | QdrantConfig
+    ] = Field(default_factory=dict)
 
     def next_step(self):
         """Move to the next step in the workflow."""
