3cdcaa29ee184c39c49ae55d0b0c3a7061660982
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2025-06-05 05:17:14 +0200

    Bump version 0.4.12 → 0.4.13

--- src/docler/__init__.py
+++ src/docler/__init__.py
@@ -18,7 +18,7 @@ from docler.converters.markitdown_provider import MarkItDownConverter
 from docler.converters.mistral_provider import MistralConverter
 from docler.converters.upstage_provider import UpstageConverter
 
-__version__ = "0.4.12"
+__version__ = "0.4.13"
 
 __all__ = [
     "AggregatedConverter",
