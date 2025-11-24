cdcedbbb3d9615439e061ad793501d2858559485
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2025-11-24 12:59:40 +0100

    chore: copier update

--- src/docler/log.py
+++ src/docler/log.py
@@ -5,6 +5,21 @@ from __future__ import annotations
 import logging
 
 
-def get_logger(name: str) -> logging.Logger:
-    """Get a logger for the given name."""
-    return logging.getLogger(f"docler.{name}")
+LogLevel = int | str
+
+
+def get_logger(name: str, log_level: LogLevel | None = None) -> logging.Logger:
+    """Get a logger for the given name.
+
+    Args:
+        name: The name of the logger.
+              Will be prefixed with 'docler'
+        log_level: The logging level to set for the logger
+
+    Returns:
+        A logger instance
+    """
+    logger = logging.getLogger(f"docler.{name}")
+    if log_level is not None:
+        logger.setLevel(log_level)
+    return logger
