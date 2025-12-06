276f80250446d1059bc84b01227aa281e81a91bb
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2025-12-06 22:07:46 +0100

    chore: rem VectorDB stuff

--- src/docler/vector_db/dbs/chroma_db/__init__.py
+++
@@ -1,6 +0,0 @@
-from __future__ import annotations
-
-from docler.vector_db.dbs.chroma_db.db import ChromaBackend
-from docler.vector_db.dbs.chroma_db.manager import ChromaVectorManager
-
-__all__ = ["ChromaBackend", "ChromaVectorManager"]
